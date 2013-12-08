540c7369b035dc478bd2a27d79678b8b5af18c8b
Roland Schuetz <mail@rolandschuetz.at>
2013-12-08 18:16:52 +0100

    Created new production build

---
+++ sencha-architect/build/temp/production/BlogApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,3 @@
+// @tag full-page
+// @require /Users/roland/Documents/AptanaWorkspace/BanchaBlogApp/app/sencha-architect/ext/ext-theme-neptune.js
+// @require /Users/roland/Documents/AptanaWorkspace/BanchaBlogApp/app/sencha-architect/app.js
