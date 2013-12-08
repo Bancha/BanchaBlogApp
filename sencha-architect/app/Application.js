aab0ee0e4d6785bef505341b4ae2010649d3176a
Roland Schuetz <mail@rolandschuetz.at>
2013-12-08 03:11:08 +0100

    Updated to Sencha Architect 3

---
+++ sencha-architect/app/Application.js
@@ -0,0 +1,17 @@
+Ext.define('BlogApp.Application', {
+    name: 'BlogApp',
+
+    extend: 'Ext.app.Application',
+
+    views: [
+        // TODO: add views here
+    ],
+
+    controllers: [
+        // TODO: add controllers here
+    ],
+
+    stores: [
+        // TODO: add stores here
+    ]
+});
---
+++ sencha-architect/app/controller/Main.js
@@ -0,0 +1,3 @@
+Ext.define('BlogApp.controller.Main', {
+    extend: 'Ext.app.Controller'
+});
