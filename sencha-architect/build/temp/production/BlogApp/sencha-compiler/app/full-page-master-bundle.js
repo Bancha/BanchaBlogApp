82c275f8e650909a5be396305cb7311bdaee3521
Roland Schuetz <mail@rolandschuetz.at>
2014-10-10 02:51:13 +0200

    Upgraded to Sencha Architet 3.0.4, moved sencha folder inside webroot

--- sencha-architect/build/temp/production/BlogApp/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,3 +0,0 @@
-// @tag full-page
-// @require /Users/roland/Documents/AptanaWorkspace/BanchaBlogApp/app/sencha-architect/ext/ext-theme-neptune.js
-// @require /Users/roland/Documents/AptanaWorkspace/BanchaBlogApp/app/sencha-architect/app.js
---
+++ webroot/sencha-architect/build/temp/production/BlogApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,3 @@
+// @tag full-page
+// @require /Users/roland/Documents/AptanaWorkspace/BanchaBlogApp/app/webroot/sencha-architect/ext/ext-theme-neptune.js
+// @require /Users/roland/Documents/AptanaWorkspace/BanchaBlogApp/app/webroot/sencha-architect/app.js
