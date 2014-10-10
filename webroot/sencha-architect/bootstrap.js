c51ffc110ee90e60ef9829bed21c5239971beb4f
Roland Schuetz <mail@rolandschuetz.at>
2014-10-10 17:19:35 +0200

    Updates to Sencha Architect 3.1 (build doesn't yet work)

--- webroot/sencha-architect/bootstrap.js
+++ webroot/sencha-architect/bootstrap.js
@@ -35,7 +35,13 @@ Ext.Loader.addClassPathMappings({
   "Bancha.scaffold.grid.override.Panel": "../../Plugin/Bancha/webroot/js/scaffold/src/grid/override/Panel.js",
   "BlogApp": "app",
   "Ext": "ext/src",
+  "Ext.DomHelper": "ext/src/dom/Helper.js",
+  "Ext.DomQuery": "ext/src/dom/Query.js",
+  "Ext.EventObjectImpl": "ext/src/EventObject.js",
   "Ext.Msg": "ext/src/window/MessageBox.js",
+  "Ext.Supports": "ext/src/Support.js",
+  "Ext.core.DomHelper": "ext/src/dom/Helper.js",
+  "Ext.core.DomQuery": "ext/src/dom/Query.js",
   "Ext.data.validator": "../../Plugin/Bancha/webroot/js/data/validator",
   "Ext.rtl.EventObjectImpl": "ext/src/rtl/EventObject.js",
   "ExtThemeNeptune.Component": "ext/ext-theme-neptune.js",
@@ -2031,9 +2037,9 @@ Ext.setVersion("ext-theme-base", "4.2.1");
 Ext.setVersion("ext-theme-neptune", "4.2.1");
 Ext.setVersion("ext-theme-neutral", "4.2.1");
 Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/Component.js");;
-Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/panel/Panel.js");;
 Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/toolbar/Toolbar.js");;
 Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/layout/component/Dock.js");;
+Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/panel/Panel.js");;
 Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/container/ButtonGroup.js");;
 Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/toolbar/Paging.js");;
 Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/picker/Month.js");;
@@ -2042,8 +2048,8 @@ Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/panel/Table.js")
 Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/grid/RowEditor.js");;
 Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/grid/column/RowNumberer.js");;
 Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/resizer/Splitter.js");;
-Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/menu/Menu.js");;
 Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/menu/Separator.js");;
+Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/menu/Menu.js");;
 Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/panel/Tool.js");;
 Ext.Loader.loadScript("ext/packages/ext-theme-neptune/overrides/tab/Tab.js");;
 /**
--- webroot/sencha-architect/build/temp/production/BlogApp/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,3 +0,0 @@
-// @tag full-page
-// @require /Users/roland/Documents/AptanaWorkspace/BanchaBlogApp/app/webroot/sencha-architect/ext/ext-theme-neptune.js
-// @require /Users/roland/Documents/AptanaWorkspace/BanchaBlogApp/app/webroot/sencha-architect/app.js
