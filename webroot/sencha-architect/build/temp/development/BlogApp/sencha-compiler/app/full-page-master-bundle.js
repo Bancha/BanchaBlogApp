bc089977bc81571e2a4b242e579384372a016f40
Roland Schuetz <mail@rolandschuetz.at>
2014-10-18 22:28:17 +0200

    Updated to Ext JS 5

---
+++ webroot/sencha-architect/build/temp/development/BlogApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/roland/Documents/AptanaWorkspace/BanchaBlogApp/app/webroot/sencha-architect/app.js
---
+++ webroot/sencha-architect/build/temp/production/BlogApp/sass/config.rb
@@ -0,0 +1,3 @@
+require '../../../../../ext/packages/ext-theme-base/sass/utils.rb'
+require '../../../../../sass/config.rb'
+cache_path = '/Users/roland/Documents/AptanaWorkspace/BanchaBlogApp/app/webroot/sencha-architect/build/.sass-cache'
